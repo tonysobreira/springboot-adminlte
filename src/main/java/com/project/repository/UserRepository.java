799aa90ab13573f1e93fa762386f504432dcb40e
Tony <tonysobreira@gmail.com>
2018-12-28 13:18:05 -0300

    Create UserRepository.java

---
+++ src/main/java/com/project/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+
+}
